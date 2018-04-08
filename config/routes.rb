b8614ef59434e8f344cee936de82ea9475b1ee5c
Kevin Waters <ec2-user@ip-172-31-24-92.us-east-2.compute.internal>
2018-04-08 19:18:42 +0000

    added login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+=======
+  
+get '/checkout', to: 'cart#index' 
+   
+  get 'cart/index'
+>>>>>>> 96be3a5d9a7544a6a19ae213dc8841f0d63508d2
 
   resources :items
   root 'static_pages#home'
@@ -10,10 +17,22 @@ Rails.application.routes.draw do
   
   get '/items', to: 'items#items'
   
+<<<<<<< HEAD
 
   get '/login', to: 'user#login'
 
   get '/logout', to: 'user#logout'
+=======
+  get '/cart', to: 'cart#index'
+  get '/cart/clear', to: 'cart#clearCart'
+   
+  get '/cart/:id', to: 'cart#add'
+  get '/cart/remove/:id', to: 'cart#remove'
+  root :to => 'site#home'
+>>>>>>> 96be3a5d9a7544a6a19ae213dc8841f0d63508d2
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+ 
+ 
