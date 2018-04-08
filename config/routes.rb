96be3a5d9a7544a6a19ae213dc8841f0d63508d2
Diana Nevedomska <diana_nevedomska@yahoo.com>
2018-04-08 15:11:16 +0000

    addingNewCart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  
+get '/checkout', to: 'cart#index' 
+   
+  get 'cart/index'
+
   resources :items
   root 'static_pages#home'
 
@@ -11,5 +14,15 @@ Rails.application.routes.draw do
   
   get '/items', to: 'items#items'
   
+  get '/cart', to: 'cart#index'
+  get '/cart/clear', to: 'cart#clearCart'
+   
+  get '/cart/:id', to: 'cart#add'
+  get '/cart/remove/:id', to: 'cart#remove'
+  root :to => 'site#home'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+ 
+ 
