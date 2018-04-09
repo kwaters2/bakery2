f953f4fbd3c686496e19a1ca4513fb6410d5d7a6
Kevin Waters <ec2-user@ip-172-31-24-92.us-east-2.compute.internal>
2018-04-09 20:05:40 +0000

    accepted merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  
+
 get '/checkout', to: 'cart#index' 
    
   get 'cart/index'
-<<<<<<< HEAD
->>>>>>> 96be3a5d9a7544a6a19ae213dc8841f0d63508d2
-=======
->>>>>>> 96be3a5d9a7544a6a19ae213dc8841f0d63508d2
 
   resources :items
   root 'static_pages#home'
@@ -19,28 +13,18 @@ get '/checkout', to: 'cart#index'
   get '/contact', to: 'static_pages#contact'
   
   get '/items', to: 'items#items'
-  
-<<<<<<< HEAD
-<<<<<<< HEAD
-
+ 
   get '/login', to: 'user#login'
 
   get '/logout', to: 'user#logout'
-=======
-=======
->>>>>>> 96be3a5d9a7544a6a19ae213dc8841f0d63508d2
+
   get '/cart', to: 'cart#index'
   get '/cart/clear', to: 'cart#clearCart'
    
   get '/cart/:id', to: 'cart#add'
   get '/cart/remove/:id', to: 'cart#remove'
   root :to => 'site#home'
-<<<<<<< HEAD
->>>>>>> 96be3a5d9a7544a6a19ae213dc8841f0d63508d2
-=======
->>>>>>> 96be3a5d9a7544a6a19ae213dc8841f0d63508d2
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
 
  
