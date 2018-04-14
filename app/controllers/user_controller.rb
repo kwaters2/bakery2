6712a32c4e81a73b519748c610a38af9e5ca93fd
Kevin Waters <ec2-user@ip-172-31-24-92.us-east-2.compute.internal>
2018-04-14 20:14:23 +0000

    Added login function

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,20 @@
 class UserController < ApplicationController
-  
-  def login
-    session[:login] = 1 
-    flash[:notice] = "Welcome!"
-    redirect_to :controller => :items 
+    
+    
+    def login
+      session[:login] = 1 
+      flash[:notice] = "Hello, welcome to the online bakery!"
+      redirect_to :controller => :items
     end
-
-
-  def logout
-    session[:login] = nil
-    flash[:notice] = "Goodbye, See you soon!"
-    redirect_to :controller => :items 
-  end
-
+    
+    
+    def logout
+       session[:login] = nil
+       flash[:notice] = "Goodbye!"
+       redirect_to :controller => :items
+       session[:cart] = nil
+    end
+    
+    
+    
 end
