8a80e61f86db05106212a49a00b135ea0910aaa7
EC2 Default User <ec2-user@ip-172-31-27-9.us-east-2.compute.internal>
2018-04-20 18:37:07 +0000

    about contact styling pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419205340) do
+ActiveRecord::Schema.define(version: 20180420085732) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
